8823d5b5606729612c57d224b9afc40204929ce8
Antti Tönkyrä <daedalus@pingtimeout.net>
2019-08-09 22:03:25 +0300

    first aksualli working version

--- devices/ciscoswitch.py
+++ devices/ciscoswitch.py
@@ -29,6 +29,8 @@ class CiscoSwitch(devices.device.Device):
                 self._write(tc, 'configure terminal')
                 self._write(tc, 'ip ssh rsa keypair-name ssh')
                 self._write(tc, 'crypto key generate rsa mod 2048 label ssh', timeout=120)
+                self._write(tc, 'sdm prefer dual-ipv4-and-ipv6 default', timeout=10)
+                self._write(tc, 'sdm prefer dual-ipv4-and-ipv6 vlan', timeout=10)
                 self._write(tc, 'end')
                 self._write(tc, 'exit')
                 self._logger.info('logged out')
@@ -45,13 +47,12 @@ class CiscoSwitch(devices.device.Device):
         self._logger.error('failed to fetch information from switch')
 
     def configure(self, switch_config):
-        time.sleep(10)
         try:
             tc = telnetlib.Telnet(self.address, timeout=3)
             self._write(tc, None, [b'\r\n[Uu]sername: '])
             self._write(tc, config.get('liscain', 'liscain_init_username'), [b'\r\n[Pp]assword: '])
             self._write(tc, config.get('liscain', 'liscain_init_password'))
-            self._logger.info('[configure] logged in')
+            self._logger.info('[configure] logged in, begin configure')
             self._write(tc, 'terminal length 0')
             self._write(tc, 'tclsh')
             tclsh_exp = [b'\\+>']
@@ -63,8 +64,14 @@ class CiscoSwitch(devices.device.Device):
             self._write(tc, 'exit')
             self._write(tc, 'copy flash:liscain.config.in startup-config', [b'\r\n'])
             self._write(tc, 'startup-config')
-            self._write(tc, 'exit')
-            self._logger.info('[configure] logged out')
+            try:
+                prompt = self._write(tc, 'reload', [b'\r\n'])
+                if 'yes/no' in prompt:
+                    self._write(tc, 'no', [b'\r\n'])
+                self._write(tc, 'confirm')
+            except socket.timeout:
+                pass
+            self._logger.info('[configure] completed')
             self.change_state(SwitchState.CONFIGURED)
         except socket.timeout:
             self._logger.info('[configure] timed out')
